39d36cf79380fddfaedb41bb3dc03ed0aedbab81
sjtommylee <sjtommylee@gmail.com>
2023-07-25 04:18:10 -0700

    chore: commit save point

---
+++ packages/app/state/theme.ts
@@ -0,0 +1,12 @@
+// state/theme.ts
+import { atom, useAtom } from "jotai";
+
+export const themeAtom = atom<"light" | "dark">("light");
+
+export function useThemeToggle() {
+  const [theme, setTheme] = useAtom(themeAtom);
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+  return { theme, toggleTheme };
+}
