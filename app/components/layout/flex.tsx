0a8a4f062509cd73f9c6779741909b60b809549c
sjtommylee <sjtommylee@gmail.com>
2024-01-25 17:22:12 -0800

    check in

---
+++ app/components/layout/flex.tsx
@@ -0,0 +1,10 @@
+export type FlexProps = {
+  row?: boolean;
+  grow?: boolean;
+  fill?: boolean;
+  centered?: boolean;
+};
+
+export const Flex: React.FC<FlexProps> = ({ row, grow, fill, centered }) => {
+  return <div>hi</div>;
+};
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+import stylexPlugin from "@stylexjs/nextjs-plugin";
+import path from "path";
+
+const __filename = new URL(import.meta.url).pathname;
+const __dirname = path.dirname(__filename);
+
+const nextConfig = {
+  pageExtensions: ["ts", "tsx", "js", "jsx"],
+  transpilePackages: [],
+};
+
+export default stylexPlugin({
+  rootDir: __dirname,
+})(nextConfig);
