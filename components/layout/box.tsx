67ffeb335491da81da3394b85858a889df7c46bb
sjtommylee <sjtommylee@gmail.com>
2024-01-28 16:26:44 -0800

    chore: commit save point

--- components/layout/box.tsx
+++ components/layout/box.tsx
@@ -1,1 +1,9 @@
-export {};
+import { ReactNode } from "react";
+
+type BoxProps = {
+  children: ReactNode;
+};
+
+export const Box: React.FC<BoxProps> = ({ children }) => {
+  return <div>{children}</div>;
+};
