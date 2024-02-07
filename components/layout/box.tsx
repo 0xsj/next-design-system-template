450d6597b698fcea26c2f04e35369f3c86ee3cd8
sjtommylee <sjtommylee@gmail.com>
2024-02-06 20:38:59 -0800

    chore: commit save point

--- components/layout/box.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-type BoxProps = {
-  children: ReactNode;
-};
-
-export const Box: React.FC<BoxProps> = ({ children }) => {
-  return <div>{children}</div>;
-};
--- env.ts
+++
@@ -1,5 +0,0 @@
-// import { z } from "zod";
-
-// const zodEnv = z.object({ NEXT_PUBLIC_SITE_URL: z.string() });
-
-// declare global {}
